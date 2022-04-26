b6c0f71dcf77d931003e37f43428b5930721ef3d
Everton Silva <evertondouglaspereira54101@gmail.com>
2022-04-26 10:59:02 -0300

    Testando

--- src/pages/HOME/index.js
+++ src/pages/HOME/index.js
@@ -11,6 +11,7 @@ function Home() {
              <div className="navbar">Navbar</div>
              <div className="feed">Feed</div>
          </main>
+         <h1>commit</h1>
      </div>
     );
   }
